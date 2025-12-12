29162907bea7cec7fccdd524968bbc61a603ec75
ShikharSharan <87129404+ShikharSharan@users.noreply.github.com>
2025-12-12 13:59:11 +0530

    changes

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     return render_template('index.html')
-
-@app.route('/providers')
-def providers():
-    # Basic placeholder for provider search functionality
-    query = request.args.get('q', '')
-    # In real app, you'd search the DB here
-    return render_template('providers.html', query=query)
-
-@app.route('/appointment')
-def appointment():
-    return render_template('appointement.html')
 
 @app.route('/about')
 def about():
@@ -26,9 +14,25 @@ def about():
 def contact():
     return render_template('contact.html')
 
+@app.route('/providers')
+def providers():
+    return render_template('providers.html')
+
+@app.route('/appointment')
+def appointment():
+    return render_template('appointement.html')
+
 @app.route('/faq')
 def faq():
     return render_template('faq.html')
+
+@app.route('/resources')
+def resources():
+    return render_template('resource.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
